c59e398579841b17ad6808906b4406bb52e0c1db
Gehan Gonsalkorale <gehan.g@gmail.com>
2014-01-12 16:00:26 +0000

    Better illustration of problem

--- test.js
+++ test.js
@@ -9,15 +9,25 @@ var Mdl = Backbone.RelationalModel.extend({
     }]
 });
 
-var feck = new Mdl({
-    children: [
-        {id: 1, text: 'hello'},
-        {id: 2, text: 'internet'}
-    ]
+var children = [
+    {id: 1, text: 'hello'},
+    {id: 2, text: 'internet'}
+];
+children.forEach(function(child){
+    ChildModel.findOrCreate(child);
 });
 
-console.log('event types after model creation', Backbone.Relational.store._collections[1]._events);
+// Should be no events on this ChildModel store before this model is created
+console.log('events types before model', Backbone.Relational.store._collections[0]._events);
+
+var feck = new Mdl({
+    children: children
+});
+
+// ChildModel store should now have various events which point to the model feck
+console.log('event types after model creation', Backbone.Relational.store._collections[0]._events);
 
 feck.trigger('relational:unregister', feck);
 
-console.log('events types after model unregister', Backbone.Relational.store._collections[1]._events);
+// ChildModel store should now have no event listeners
+console.log('events types after model unregister', Backbone.Relational.store._collections[0]._events);
